f9ffcc7b37910af6399272a3a0ae4d096490cfb1
I Putu Andika Prasetya <114897061+roronoazororill@users.noreply.github.com>
2024-07-10 01:55:21 +0800

    Update BarberModel.kt
    Hi

--- app/src/main/java/com/example/elevenbarbershop/model/BarberModel.kt
+++ app/src/main/java/com/example/elevenbarbershop/model/BarberModel.kt
@@ -3,7 +3,46 @@ package com.example.elevenbarbershop.model
 import android.os.Parcel
 import android.os.Parcelable
 
+data class BarberModel(
+    val name: String = "",
+    val id: Int = 0,
+    val bPict: String = "",
+    val loc: String = "",
+    val des: String = "",
+    val skill: String = "",
+    val lg: String = ""
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString() ?: "",
+        parcel.readInt(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
 
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeInt(id)
+        parcel.writeString(bPict)
+        parcel.writeString(loc)
+        parcel.writeString(des)
+        parcel.writeString(skill)
+        parcel.writeString(lg)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<BarberModel> {
+        override fun createFromParcel(parcel: Parcel): BarberModel {
+            return BarberModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<BarberModel?> {
+            return arrayOfNulls(size)
         }
     }
 }
